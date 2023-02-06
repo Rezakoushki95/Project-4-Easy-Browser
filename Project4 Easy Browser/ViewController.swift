8a4cc9f2010f0f88ece58ccd6afadd2980ce16c9
rezakoushki95 <rezaaarhus@hotmail.com>
2023-02-06 04:59:58 +0100

    use webView function decidePolicyFor to check the urls the user is clicking to (besides the ones presented through the Alert Controller) if the urls cointains apple.com or hackingwithswift.com then allow, else don't.

--- Project4 Easy Browser/ViewController.swift
+++ Project4 Easy Browser/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
 	
 	var webView: WKWebView!
 	var progressView: UIProgressView!
+	var websites = ["apple.com", "hackingwithswift.com"]
 	
 	override func loadView() {
 		super.loadView()
@@ -22,11 +23,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		webView = WKWebView()
-		webView.navigationDelegate = self
-		view = webView
-		
+				
 		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
 		navigationController?.navigationBar.backgroundColor = .white
 		
@@ -43,7 +40,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		
 		webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
 		
-		let url = URL(string: "https://www.hackingwithswift.com")!
+		let url = URL(string: "https://" + websites[0])!
 		webView.load(URLRequest(url: url))
 		webView.allowsBackForwardNavigationGestures = true
 		
@@ -52,8 +49,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
 	
 	@objc func openTapped() {
 		let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
-		ac.addAction(UIAlertAction(title: "Apple.com", style: .default, handler: openPage))
-		ac.addAction(UIAlertAction(title: "HackingWithSwift.com", style: .default, handler: openPage))
+		
+		for website in websites {
+			ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+		}
+		
 		ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
 		ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
 		present(ac, animated: true)
@@ -73,6 +73,20 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		if keyPath == "estimatedProgress" {
 			progressView.progress = Float(webView.estimatedProgress)
 		}
+	}
+	
+	func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+		let url = navigationAction.request.url
+		if let host = url?.host {
+			for website in websites {
+				if host.contains(website) {
+					decisionHandler(.allow)
+					return
+				}
+			}
+			
+		}
+		decisionHandler(.cancel)
 	}
 }
 
